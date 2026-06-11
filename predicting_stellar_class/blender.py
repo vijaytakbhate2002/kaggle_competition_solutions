f22193424d5e1307182148cc650d1ac1803147db
Vijay Dipak Takbhate <vijaytakbhate20@gmail.com>
2026-06-11 22:55:47 +0530

    Finalized LightGBM Solution

--- predicting_stellar_class/blender.py
+++ predicting_stellar_class/blender.py
@@ -1,11 +1,41 @@
+# import pandas as pd
+
+# sub1 = pd.read_csv("probabilities\\lgb_07_06_2026_v1.csv")
+# sub2 = pd.read_csv("probabilities\\lgb_07_06_2026_v2.csv")
+
+# weights = [0.96597, 0.96579]
+# w1 = weights[0] / sum(weights)
+# w2 = weights[1] / sum(weights)
+
+# prob_cols = ["GALAXY", "QSO", "STAR"]
+
+# blend = pd.DataFrame()
+# blend["id"] = sub1["id"]
+
+# # Blend probabilities
+# for col in prob_cols:
+#     blend[col] = w1 * sub1[col] + w2 * sub2[col]
+
+# # Final prediction as string
+# blend["class"] = blend[prob_cols].idxmax(axis=1)
+# submission = blend[["id", "class"]]
+# submission.to_csv("submissions\\blends\\blend_lgb_sub_07_06_2026_v1_lgb_sub_10_06_2026_v2.csv",index=False)
+
+
+
+
 import pandas as pd
 
 sub1 = pd.read_csv("probabilities\\lgb_07_06_2026_v1.csv")
 sub2 = pd.read_csv("probabilities\\lgb_07_06_2026_v2.csv")
+sub3 = pd.read_csv("probabilities\\lgb_10_06_2026_v1.csv")
+sub4 = pd.read_csv("probabilities\\lgb_10_06_2026_v2.csv")
 
-weights = [0.96597, 0.96579]
+weights = [0.96597, 0.96579, 0.96656, 0.96611]
 w1 = weights[0] / sum(weights)
 w2 = weights[1] / sum(weights)
+w3 = weights[2] / sum(weights)
+w4 = weights[3] / sum(weights)
 
 prob_cols = ["GALAXY", "QSO", "STAR"]
 
@@ -14,9 +44,9 @@ blend["id"] = sub1["id"]
 
 # Blend probabilities
 for col in prob_cols:
-    blend[col] = w1 * sub1[col] + w2 * sub2[col]
+    blend[col] = w1 * sub1[col] + w2 * sub2[col] + w3 * sub3[col] + w4 * sub4[col]
 
 # Final prediction as string
 blend["class"] = blend[prob_cols].idxmax(axis=1)
 submission = blend[["id", "class"]]
-submission.to_csv("submissions\\blends\\blend_lgb_sub_10_06_2026_v1_lgb_sub_10_06_2026_v2.csv",index=False)
+submission.to_csv("submissions\\blends\\blend_lgb_sub_07_06_2026_v1_lgb_sub_10_06_2026_v2_lgb_sub_10_06_2026_v1_lgb_sub_10_06_2026_v2.csv",index=False)
---
+++ predicting_stellar_class/temp_params.py
@@ -0,0 +1,14 @@
+# best_lgb_params = {
+#                     'n_estimators': 1183, 'learning_rate': 0.04197493542396162, 'num_leaves': 49, 'max_depth': 10, 'min_child_samples': 17, 
+#                     'subsample': 0.7637166011601102, 'colsample_bytree': 0.6567829239404623, 'reg_alpha': 0.8967987696423018, 
+#                     'reg_lambda': 7.538568226432925, 'min_split_gain': 0.6697035110875358
+#                    }
+
+# best_lgb_params = {'n_estimators': 1765, 'learning_rate': 0.010488755966065043, 'num_leaves': 124, 'max_depth': 13, 'min_child_samples': 6, 'subsample': 0.8565711988177247, 'colsample_bytree': 0.8170007643681162, 'reg_alpha': 0.5114444055800705, 'reg_lambda': 0.007243721824937548, 'min_split_gain': 0.7325616646035212}
+
+# best_lgb_params = {
+#                     'n_estimators': 1841, 'learning_rate': 0.016121467631636684, 'num_leaves': 126, 
+#                     'max_depth': 12, 'min_child_samples': 7, 'subsample': 0.9137011411523339, 
+#                     'colsample_bytree': 0.6472035788104408, 'reg_alpha': 1.6143965568012781, 
+#                     'reg_lambda': 0.0457226857453295, 'min_split_gain': 0.520062046419683
+#                     } # 0.965875
